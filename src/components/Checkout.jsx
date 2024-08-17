6d5d5aa6adbd0aba9be2dff2e9f8888ade14e33d
shanME <giorgie103188@gmail.com>
2024-08-17 21:35:04 +0400

    cart and checkout open-close bug fixing

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formating";
+import Input from "./UI/Input";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+
+const Checkout = () => {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (acc, cur) => acc + cur.quantity * cur.price,
+    0
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+
+        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="E-Mail Address" id="email" type="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="coty" />
+        </div>
+
+        <p className="modal-actions">
+          <Button onClick={handleClose} type="button" textOnly>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
+        </p>
+      </form>
+    </Modal>
+  );
+};
+
+export default Checkout;
