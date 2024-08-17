f9a8e44bf872bd2d49969da15da26a2d2e91cefe
shanME <giorgie103188@gmail.com>
2024-08-17 19:11:38 +0400

    end context

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -6,10 +6,70 @@ const CartContext = createContext({
   removeItem: (id) => {},
 });
 
-export function CartContextProvider({ children }) {
-  const [] = useReducer();
+function cartReducer(state, action) {
+  if (action.type === "ADD") {
+    const existimgCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const updatedItems = [...state.items];
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+    if (existimgCartItemIndex > -1) {
+      const existingItem = state.items[existimgCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existimgCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE") {
+    const existimgCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existimgCartItemIndex];
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existimgCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existimgCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;
