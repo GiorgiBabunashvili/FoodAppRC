ddadfe0da2f2d37da4ae8b1685886cd706f44bfc
shanME <giorgie103188@gmail.com>
2024-08-17 19:38:15 +0400

    modal context

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
-const Modal = () => {
-  return <dialog></dialog>;
+const Modal = ({ children, open, className = "" }) => {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
 };
 
 export default Modal;
